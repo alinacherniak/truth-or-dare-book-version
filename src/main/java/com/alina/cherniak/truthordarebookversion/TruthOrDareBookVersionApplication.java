4b0cd8a91bbcba915d294144a32555a2495d1266
alinacherniak <alinachernyak.job@gmail.com>
2021-02-03 01:49:06 +0200

    Initial commit

---
+++ src/main/java/com/alina/cherniak/truthordarebookversion/TruthOrDareBookVersionApplication.java
@@ -0,0 +1,13 @@
+package com.alina.cherniak.truthordarebookversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruthOrDareBookVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruthOrDareBookVersionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alina/cherniak/truthordarebookversion/TruthOrDareBookVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alina.cherniak.truthordarebookversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruthOrDareBookVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
